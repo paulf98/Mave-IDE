3ae015a664d280851f8384a156a2efb4df1ff54d
Paul Faass <paulfaass@gmail.com>
2020-08-28 10:30:53 +0200

    initial commit

---
+++ mave-branding/src/browser/mave-branding-frontend-module.ts
@@ -0,0 +1,6 @@
+import './branding';
+import '../../src/browser/style/index.css';
+
+import { ContainerModule } from 'inversify';
+
+export default new ContainerModule((bind, unbind, isBound, rebind) => {});
