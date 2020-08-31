301c2ec41c4c6073020fdc30b80624a6a62bcbc6
Paul Faass <paulfaass@gmail.com>
2020-08-31 10:43:52 +0200

    Added mave-doc extension

---
+++ mave-doc/src/browser/mave-doc-contribution.ts
@@ -0,0 +1,35 @@
+import { injectable, inject } from 'inversify';
+import {
+	CommandContribution,
+	CommandRegistry,
+	MenuContribution,
+	MenuModelRegistry,
+	MessageService
+} from '@theia/core/lib/common';
+import { CommonMenus } from '@theia/core/lib/browser';
+
+export const MaveDocCommand = {
+	id: 'MaveDoc.command',
+	label: 'Documentation'
+};
+
+@injectable()
+export class MaveDocCommandContribution implements CommandContribution {
+	constructor(@inject(MessageService) private readonly messageService: MessageService) {}
+
+	registerCommands(registry: CommandRegistry): void {
+		registry.registerCommand(MaveDocCommand, {
+			execute: () => this.messageService.info('Hello World!')
+		});
+	}
+}
+
+@injectable()
+export class MaveDocMenuContribution implements MenuContribution {
+	registerMenus(menus: MenuModelRegistry): void {
+		menus.registerMenuAction(CommonMenus.HELP, {
+			commandId: MaveDocCommand.id,
+			label: MaveDocCommand.label
+		});
+	}
+}
