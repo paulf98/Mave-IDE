16ee0e212f2c8e60bbf7ff6de341c7092ff9bbda
Paul Faass <paulfaass@gmail.com>
2020-09-08 13:01:22 +0200

    Implemented Mini-Browser Extension for opening a web page inside of Theia

--- mave-doc/src/browser/mave-doc-contribution.ts
+++ mave-doc/src/browser/mave-doc-contribution.ts
@@ -1,27 +1,23 @@
 import { injectable, inject } from 'inversify';
-import {
-	CommandContribution,
-	CommandRegistry,
-	MenuContribution,
-	MenuModelRegistry,
-	MessageService
-} from '@theia/core/lib/common';
+import { CommandContribution, CommandRegistry, MenuContribution, MenuModelRegistry } from '@theia/core/lib/common';
 import { CommonMenus } from '@theia/core/lib/browser';
+import { MiniBrowserOpenHandler } from '@theia/mini-browser/lib/browser/mini-browser-open-handler';
+import URI from '@theia/core/lib/common/uri';
 
 export const MaveDocCommand = {
 	id: 'MaveDoc.command',
 	label: 'Documentation'
 };
 
+const link = new URI('https://www.hs-kl.de/');
 @injectable()
 export class MaveDocCommandContribution implements CommandContribution {
-	constructor(@inject(MessageService) private readonly messageService: MessageService) {}
+	constructor(@inject(MiniBrowserOpenHandler) private readonly miniBrowserOpenHandler: MiniBrowserOpenHandler) {}
 
 	registerCommands(registry: CommandRegistry): void {
 		registry.registerCommand(MaveDocCommand, {
 			execute: () => {
-				console.log('Start mini-browser');
-				this.messageService.info('Hello World!');
+				this.miniBrowserOpenHandler.open(link);
 			}
 		});
 	}
